70d3b7d64aa70c2764f7989128bc0751b865605e
Cristian Román Palacios <cromanpa94@gmail.com>
2023-05-31 11:23:08 -0700

    Figures updated

--- Figures/Fig1.R
+++ Figures/Fig1.R
@@ -8,47 +8,71 @@ ggthemr('light')
 
 ds <- here::here("Analyses", "Results", "Petersen")
 datasets <- rio::import_list(list.files(ds, full.names = T))
-datasets <- datasets[grep(" CI", names(datasets))]
-datasets <- rbindlist(datasets, idcol = "Model", fill = TRUE)
+datasetsP <- datasets[-grep(" CI", names(datasets))]
+
+ds <- here::here("Analyses", "Results", "Anderson_Full")
+datasets <- rio::import_list(list.files(ds, full.names = T))
+datasetsA <- datasets[-grep(" CI", names(datasets))]
+
+
+datasetsP <- rbindlist(datasetsP, idcol = "Model", fill = TRUE)
+datasetsA <- rbindlist(datasetsA, idcol = "Model", fill = TRUE)
+
+datasets <- rbindlist(list("Petersen" = datasetsP,
+                           "Anderson" = datasetsA), 
+                      idcol = "Dataset", 
+                      fill = TRUE)
+
 
 datasets$Model <- factor(datasets$Model,
-                               levels = c("Bayesian model no errors CI", 
-                                          "Bayesian model with errors CI",
-                                          "Bayesian mixed w errors CI",
-                                          "Linear regression CI",
-                                          "Inverse linear regression CI",
-                                          "Deming regression CI", 
-                                          "York regression CI") ,
+                               levels = c("Bayesian model no errors", 
+                                          "Bayesian model with errors",
+                                          "Bayesian mixed w errors",
+                                          "Linear regression",
+                                          "Inverse linear regression",
+                                          "Deming regression", 
+                                          "York regression") ,
                                labels = 
                                  c("B-SL", "B-SL-E", "B-LMM", "OLS", "W-OLS", "D", "Y")
 )
 
+datasets <- datasets[,c("Dataset", "Model", "alpha", "beta")]
+datasets <- na.omit(datasets)
 
-Petersen <- bayclumpr::cal.ci(data.frame(alpha=rnorm(1000, 0.258, 1.70E-05),
-                                          beta=rnorm(1000, 0.0383, 1.70E-06)), 0.8,13.6)
+aggregated <- aggregate(. ~ Dataset + Model, datasets, function(x) c(mean = mean(x), sd = sd(x)/sqrt(length(x))))
+aggregated <- data.frame(aggregated)
+aggregated <- do.call("data.frame", aggregated)
+
+reference <- data.frame(Dataset = c("Petersen","Anderson"), 
+           Model = "Reference", 
+           alpha.mean = c(0.258, 0.154),   
+           alpha.sd = c(1.70E-05, 4.00E-03), 
+           beta.mean  = c(0.0383, 0.0391) ,   
+           beta.sd = c(1.70E-06, 4.00E-04)
+           )
+
+aggregated <- rbind(aggregated, reference)
 
 
-datasets <- datasets[datasets$Model  != "B-LMM",]
-
-colnames(datasets)[2] <- "Temperature"
-regs <- datasets
+aggregated$Dataset <- factor(aggregated$Dataset,
+                         levels = c("Petersen", 
+                                    "Anderson")
+                         )
+aggregated$fishape <- ifelse(aggregated$Model == "Reference", 1, 16)
 
 p1 <- 
-  ggplot(data=regs) +
-  geom_ribbon(data=Petersen[[1]],aes(x=x, y = median_est, ymin = ci_lower_est,
-                                     ymax = ci_upper_est), color=NA, fill = "darkblue",
-              alpha = 0.5)+
-   geom_ribbon(data=regs,aes(x=Temperature, y = D47_median_est, ymin = D47_ci_lower_est,
-                            ymax = D47_ci_upper_est), fill = "grey",
-              alpha = 0.5)+
-  geom_line(data=regs,aes(x=Temperature, y = D47_median_est), color= 'black')+
-  geom_line(data=Petersen[[1]],
-            aes(x=x, y = median_est), color="red", lty="dashed")+
-
-  facet_grid(.~Model) +
-  ylab(expression(Delta["47"]*" (‰)" ))+ 
-  xlab(expression(paste(10^6, " / T"^2, "(Temperature in K)")))+ 
-  guides(color="none", fill='none')+ 
+  ggplot(aggregated, aes(x = alpha.mean, y = beta.mean, color = Model)) + 
+  geom_errorbar(aes(ymin = beta.mean - beta.sd,ymax = beta.mean + beta.sd), size = .6) + 
+  geom_errorbarh(aes(xmin = alpha.mean - alpha.sd,xmax = alpha.mean + alpha.sd), size =.6) +
+  geom_point(shape = aggregated$fishape, size = 2)+
+  facet_wrap(~(Dataset), scales = "free") +
+  ylab(expression(beta))+ 
+  xlab(expression(alpha))+ 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), #axis.line = element_line(colour = "black"),
+        axis.ticks = element_line(colour = "black"),
+        panel.border = element_rect(color = "black", fill = NA, size = 1)
+  )+ theme(text = element_text(size = 22))  +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         axis.title.x = element_text(colour = "black"),
         axis.title.y = element_text(colour = "black"),
@@ -58,21 +82,22 @@ p1 <-
         panel.border = element_rect(colour = "black", fill=NA),
         axis.line.x.bottom=element_line(color="black", size=0.1),
         axis.line.y.left=element_line(color="black", size=0.1),
-        text = element_text(size=15))+
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))+
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 10))+
-  theme(axis.text.x = element_text(colour="black"), 
-        axis.text.y = element_text(colour="black"))+ 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.ticks = element_line(colour = "black")
-  )+ theme(text = element_text(size = 17))
+        legend.key = element_rect(fill = "white"),
+        legend.text=element_text(color="black",size=15),
+        legend.key.size = unit(7,"point"), 
+        legend.title=element_blank())+ 
+  theme(legend.position="bottom") +
+  guides(colour = guide_legend(nrow = 1))+ 
+  scale_color_brewer(palette = "Dark2")
+
+p1
 
 
-ggsave(plot = p1, filename= here::here("Figures","Plots",'Fig9.pdf'), device= cairo_pdf, width =  15, height =  4)
+
+ggsave(plot = p1, filename= here::here("Figures","Plots",'Fig1.pdf'), device= cairo_pdf, width =  12, height =  6)
 
 
-jpeg(here::here("Figures","Plots","Fig9.jpg"), 15, 4, units = "in", res=300)
+jpeg(here::here("Figures","Plots","Fig1.jpg"), 12, 6, units = "in", res=300)
 print(p1)
 dev.off()
 
--- Figures/Fig2.R
+++ Figures/Fig2.R
@@ -6,142 +6,57 @@ library(lemon)
 library(ggthemr)
 ggthemr('light')
 
-ds <- here::here("Analyses", "Results", "Anderson_Full")
-AndDS <- read.csv(here::here("Analyses", "Datasets", "RW_Anderson_Full.csv"))
+ds <- list.files(here::here("Analyses", "Results", "Sun"), full.names = TRUE)
+datasets <- rio::import_list(ds)
+datasets <- rbindlist(datasets, idcol = "Model")
+colnames(datasets)[6] <- 'SD'
 
 
-datasets <- rio::import_list(list.files(ds, full.names = T))
-datasets <- datasets[grep(" CI", names(datasets))]
-datasets <- rbindlist(datasets, idcol = "Model", fill = TRUE)
+#write.csv(datasets, here::here("Figures", "OriginalSun.csv"))
+
+ds_sun <- read.csv(here::here("Figures", "OriginalSun.csv"), row.names = 1)
+colnames(ds_sun) <- colnames(datasets)
+
+fullDS <- rbindlist(list(datasets, ds_sun))
 
 
-datasets$Model <- factor(datasets$Model,
-                         levels = c("Bayesian model no errors CI", 
-                                    "Bayesian model with errors CI",
-                                    "Bayesian mixed w errors CI",
-                                    "Linear regression CI",
-                                    "Inverse linear regression CI",
-                                    "Deming regression CI", 
-                                    "York regression CI") ,
+fullDS$Sample <- ifelse(fullDS$`Δ47 (‰)` == 0.706, "~6.2 Ma", "0")
+fullDS$Temperature <- fullDS$`Temperature (°C)`
+
+
+fullDS <- fullDS[fullDS$Model != "Petersen et al. (2019)",]
+
+fullDS$Model <- factor(fullDS$Model,
+                         levels = c("Bayesian linear model", "Bayesian linear model, errors", "Bayesian linear mixed model" , "Linear" , "Inverse linear", "Deming", "York" , "Henkes et al. (2013)" , "Tripati et al. (2015)", "Petersen et al. (2019)"   ) ,
                          labels = 
-                           c("B-SL", "B-SL-E", "B-LMM", "OLS", "W-OLS", "D", "Y")
+                           c("B-SL", "B-SL-E", "B-LMM", "OLS", "W-OLS", "D", "Y", "H-2013", "T-2015", "P-2019")
 )
 
-datasets <- datasets[! datasets$Model == "B-LMM",]
-
-datasets$material <- ifelse(is.na(datasets$material), "Full", datasets$material)
-
-#Subset per material
-
-#range1 <- range(AndDS[AndDS$Material == 1,"Temperature"])
-#range2 <- range(AndDS[AndDS$Material == 2,"Temperature"])
-
-#Mat1 <- datasets[datasets$material == 1,]
-#Mat2 <- datasets[datasets$material == 2,]
-#MatFull <- datasets[datasets$material == "Full",]
-
-#Mat1 <- Mat1[between(unlist(Mat1[,2]), range1[1], range1[2] ),]
-#Mat2 <- Mat2[between(unlist(Mat2[,2]), range2[1], range2[2] ),]
-#datasets <- rbind.data.frame(MatFull, Mat1, Mat2)
-
-##
-
-
-
-Anderson <- bayclumpr::cal.ci(data.frame(alpha=rnorm(1000, 0.154, 4.00E-03),
-                                          beta=rnorm(1000, 0.0391, 4.00E-04)), 
-                               0.5,13.3)
-regs <- datasets
-colnames(regs)[2] <- "Temperature"
-
-
-p1 <- 
-  ggplot(data=regs) +
-  geom_ribbon(data=Anderson[[1]],aes(x=x, y = median_est, ymin = ci_lower_est,
-                                     ymax = ci_upper_est), color=NA, fill="darkblue",
-              alpha = 0.5)+
-  geom_ribbon(data=regs,aes(x=Temperature, y = D47_median_est, ymin = D47_ci_lower_est,
-                            ymax = D47_ci_upper_est), fill = "grey",
-              alpha = 0.5)+
-  geom_line(data=regs,aes(x=Temperature, y = D47_median_est), color = ' black')+
-  geom_line(data=Anderson[[1]],
-            aes(x=x, y = median_est), color="red", lty="dashed")+
-
-  facet_grid(.~Model) +
-  ylab(expression(Delta["47"]*" (‰)" ))+ 
-  xlab(expression(paste(10^6, " / T"^2, "(Temperature in K)")))+ 
-  guides(color="none", fill='none')+ 
+p1 <- ggplot(data=fullDS) +
+  geom_errorbar(aes(x=Model, ymin = Temperature-SD,ymax = Temperature+SD, color=factor(Sample)),
+                position=position_dodge(width=0.9), width=0.5, size=.7)+
+  geom_point(aes(y=Temperature, x=Model, color=factor(Sample)), position=position_dodge(width=0.9))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         axis.title.x = element_text(colour = "black"),
         axis.title.y = element_text(colour = "black"),
         axis.text.y = element_text(colour="black"),
-        axis.text.x = element_text(colour="black"),
+        axis.text.x = element_text(colour="black",
+                                   angle = 90, vjust = 0.5, hjust=1),
         strip.text = element_text(colour = 'black'),
         panel.border = element_rect(colour = "black", fill=NA),
         axis.line.x.bottom=element_line(color="black", size=0.1),
-        axis.line.y.left=element_line(color="black", size=0.1),
-        text = element_text(size=15))+
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))+
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 10))+
-  theme(axis.text.x = element_text(colour="black"), 
-        axis.text.y = element_text(colour="black"))+ 
+        axis.line.y.left=element_line(color="black", size=0.1))+
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 6))+
+  xlab("") + scale_color_manual(values=c("#62bba5", "orange"))+labs(color="Age (Ma)")+ 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.ticks = element_line(colour = "black")
-  ) + theme(text = element_text(size = 17))
+  panel.background = element_blank(), axis.line = element_line(colour = "black"),
+  axis.ticks = element_line(colour = "black")
+  )+ theme(text = element_text(size = 20))+ ylab("Temperature (°C)")
 
 
+ggsave(plot = p1, filename= here::here("Figures","Plots",'Fig2.pdf'), device= cairo_pdf, width =  15, height =  5)
 
-
-p2 <- 
-  ggplot(data=regs) +
-  geom_ribbon(data=Anderson[[1]],aes(x=sqrt(10^6/x)-273.15, y = median_est, ymin = ci_lower_est,
-                                     ymax = ci_upper_est), color=NA, fill="darkblue",
-              alpha = 0.5)+
-  geom_ribbon(data=regs,aes(x=sqrt(10^6/Temperature)-273.15, y = D47_median_est, ymin = D47_ci_lower_est,
-                            ymax = D47_ci_upper_est), fill = "grey",
-              alpha = 0.5)+
-  geom_line(data=regs,aes(x=sqrt(10^6/Temperature)-273.15, y = D47_median_est), color = ' black')+
-  geom_line(data=Anderson[[1]],
-            aes(x=sqrt(10^6/x)-273.15, y = median_est), color="red", lty="dashed")+
-  
-  facet_grid(.~Model) +
-  ylab(expression(Delta["47"]*" (‰)" ))+ 
-  xlab(expression(paste("T", "(Temperature in °C)")))+ 
-  guides(color="none", fill='none')+ 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        axis.title.x = element_text(colour = "black"),
-        axis.title.y = element_text(colour = "black"),
-        axis.text.y = element_text(colour="black"),
-        axis.text.x = element_text(colour="black"),
-        strip.text = element_text(colour = 'black'),
-        panel.border = element_rect(colour = "black", fill=NA),
-        axis.line.x.bottom=element_line(color="black", size=0.1),
-        axis.line.y.left=element_line(color="black", size=0.1),
-        text = element_text(size=15))+
-  scale_y_continuous(breaks = scales::pretty_breaks(n = 10))+
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 5))+
-  theme(axis.text.x = element_text(colour="black"), 
-        axis.text.y = element_text(colour="black"))+ 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"),
-        axis.ticks = element_line(colour = "black")
-  ) + theme(text = element_text(size = 17))
-
-
-
-ggsave(plot = p1, filename= here::here("Figures","Plots",'Fig2.pdf'), device= cairo_pdf, width =  15, height =  4)
-
-
-jpeg(here::here("Figures","Plots",'Fig2.jpg'), 15, 4, units = "in", res=300)
+jpeg(here::here("Figures","Plots",'Fig2.jpg'), 10, 5, units = "in", res=300)
 print(p1)
 dev.off()
-
-ggsave(plot = p2, filename= here::here("Figures","Plots",'Fig2_NL.pdf'), device= cairo_pdf, width =  15, height =  4)
-
-
-jpeg(here::here("Figures","Plots",'Fig2_NL.jpg'), 15, 4, units = "in", res=300)
-print(p2)
-dev.off()
-
 
